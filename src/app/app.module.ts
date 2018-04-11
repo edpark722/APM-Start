5893c1b9354d98c852552b61763d8467aa6ad755
Edmund Park <edmundpark86@gmail.com>
2018-04-11 16:22:04 -0700

    new Repository Added

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { WelcomeComponent } from './home/welcome.component';
+import { RouterModule } from '@angular/router';
+import { ProductModule } from './products/product.module';
+
+
+@NgModule({
+  //we define which of our components belong to this module.
+  declarations: [
+    AppComponent,
+    WelcomeComponent
+  ],
+  //We define the external modules that we want available to all components in this module
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {path: 'welcome', component: WelcomeComponent},
+      {path: '', redirectTo: 'welcome', pathMatch: 'full'},
+      {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    ]),
+    ProductModule
+  ],
+  //defines the startup component of the application
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+
